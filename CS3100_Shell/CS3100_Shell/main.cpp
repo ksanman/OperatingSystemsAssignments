2b24e6741cc5c475d522ac46791f4aabe7788594
kody <agent.xaber@gmail.com>
2016-09-30 11:15:16 -0600

    Added the execvp function to the main shell main file.

--- CS3100_Shell/CS3100_Shell/main.cpp
+++ CS3100_Shell/CS3100_Shell/main.cpp
@@ -34,23 +34,23 @@ int main() {
 		
 		while (!inputQueue.empty())
 		{
-			std::string workingString = inputQueue.front();
+			std::string commandString = inputQueue.front();
 			historyQueue.push_back(inputQueue);
 			inputQueue.pop();
 
-			if (workingString == "exit")
+			if (commandString == "exit")
 			{
 				clearQueue(inputQueue);
 				return 0;
 			}
-			else if (workingString == "ptime")
+			else if (commandString == "ptime")
 			{
 				std::chrono::time_point<std::chrono::high_resolution_clock> end = std::chrono::high_resolution_clock::now();
 				std::chrono::duration<double> time = end - start;
 				std::cout << "Time spent executing processes: " << time.count() << " seconds" << std::endl;
 				clearQueue(inputQueue);
 			}
-			else if (workingString == "history")
+			else if (commandString == "history")
 			{
 				for each (auto var in historyQueue)
 				{
@@ -63,13 +63,13 @@ int main() {
 				}
 				clearQueue(inputQueue);
 			}
-			else if (workingString.find('^') != std::string::npos)
+			else if (commandString.find('^') != std::string::npos)
 			{
 				std::string index = "";
 				int findIndex = 0;
-				for (int i = 1; i < workingString.size(); ++i)
+				for (int i = 1; i < commandString.size(); ++i)
 				{
-					index += workingString[i];
+					index += commandString[i];
 				}
 
 				findIndex = std::stoi(index);
@@ -80,7 +80,31 @@ int main() {
 			{
 				/* Attempt to execute a command. If we fail, display Invaild Input */
 
+				// Set the size of the argv array size. If there is nothing in the queue, it is one. If there are objects in the queue, it is the queue size plus one. 
+				auto argvSize = (inputQueue.size() > 0) ? inputQueue.size() + 1 : 1;
 
+				// Instantiate the argv char array.
+				char** argv = new char*[argvSize + 1];
+
+				// Set the first position of the argv char array to the commandString command.
+				argv[0] = new char[commandString.size()];
+				strcpy(argv[0], commandString.c_str());	
+
+				// for each element in the queue, if the exist, add it to the argv char array. 
+				for (int i = 1; i < argvSize; ++i)
+				{
+					argv[i] = new char[inputQueue.front().size()];
+					strcpy(argv[i], inputQueue.front().c_str());
+					inputQueue.pop();
+				}
+
+				// Set the last position to null. 
+				argv[argvSize] = NULL;
+
+				// Call execvp
+				execvp(argv[0], argv);
+
+				// If that fails, something went wrong. Clear the queue and display the error message. 
 				clearQueue(inputQueue);
 				std::cout << "Invalid Input" << std::endl;
 			}
